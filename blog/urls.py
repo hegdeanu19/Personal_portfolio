fc453901b480333ca9b9b79c9b7b59ec7ba24f7b
LENOVO <hegdenagashree19@gmail.com>
2022-02-23 23:33:17 -0800

    good

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+
+
+urlpatterns = [
+    
+    path('',views.all_blogs,name="all_blogs"),
+    
+]
