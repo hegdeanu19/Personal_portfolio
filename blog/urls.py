03a40e2e4129ce25fe699f9e1ee7efdffd7ae093
LENOVO <hegdenagashree19@gmail.com>
2022-02-24 23:54:12 -0800

    nice

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-
+app_name='blog'
 
 urlpatterns = [
     
     path('',views.all_blogs,name="all_blogs"),
+    path('<int:blog_id>/',views.detail,name="detail"),
+
     
 ]
